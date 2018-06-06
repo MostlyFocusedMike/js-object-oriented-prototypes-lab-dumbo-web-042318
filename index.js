807ea82be840095db494f6b702c81d86cff310e3
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-06-05 23:27:05 -0400

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,15 @@
 //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 //   }
 // }
+//
+function BoardMember(name, homeState, training) {
+  this.name = name, this.homeState = homeState, this.training = training;
+}
+
+BoardMember.prototype = {
+  veto: () => "No, I must disagree",
+  approve: () => "You can do that!",
+  doCharity: () => "I like to help people.",
+  releasePressStatement: () => "You will see great things from Scuber.",
+  sayHi: function(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
+}
